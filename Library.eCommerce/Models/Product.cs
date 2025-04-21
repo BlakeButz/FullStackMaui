c600380d94c4aab0af44f43afd4b1b462457dc31
Blake Butz <blakewbutz@gmail.com>
2025-04-21 11:53:05 -0400

    Clean commit of my project

---
+++ Library.eCommerce/Models/Product.cs
@@ -0,0 +1,54 @@
+﻿using Library.eCommerce.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spring2025_Samples.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Display
+        {
+            get
+            {
+                return $"{Id}. {Name}";
+            }
+        }
+
+        public string LegacyProperty1 { get; set; }
+        public string LegacyProperty2 { get; set; }
+        public string LegacyProperty3 { get; set; }
+        public string LegacyProperty4 { get; set; }
+        public string LegacyProperty5 { get; set; }
+        public string LegacyProperty6 { get; set; }
+
+        public Product()
+        {
+            Name = string.Empty;
+        }
+
+        public Product(Product p)
+        {
+            Name = p.Name;
+            Id = p.Id;
+        }
+
+        public override string ToString()
+        {
+            return Display ?? string.Empty;
+        }
+
+        public Product(ProductDTO p)
+        {
+            Name = p.Name;
+            Id = p.Id;
+            LegacyProperty1 = string.Empty;
+        }
+    }
+}
---
+++ Maui.eCommerce/ViewModels/ProductViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Library.eCommerce.Models;
+using Library.eCommerce.Services;
+using Spring2025_Samples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui.eCommerce.ViewModels
+{
+    public class ProductViewModel
+    {
+        private Item? cachedModel { get; set; }
+        public string? Name { 
+            get
+            {
+                return Model?.Product?.Name ?? string.Empty;
+            }
+
+            set
+            {
+                if(Model != null && Model.Product?.Name != value)
+                {
+                    Model.Product.Name = value;
+                }
+            }
+        }
+
+        public int? Quantity
+        {
+            get
+            {
+                return Model?.Quantity;
+            }
+
+            set
+            {
+                if( Model != null && Model.Quantity != value)
+                {
+                    Model.Quantity = value;
+                }
+            }
+        }
+
+        public Item? Model { get; set; }
+
+        public void AddOrUpdate()
+        {
+            ProductServiceProxy.Current.AddOrUpdate(Model);
+        }
+
+        public void Undo()
+        {
+            ProductServiceProxy.Current.AddOrUpdate(cachedModel);
+        }
+
+        public ProductViewModel() {
+            Model = new Item();
+            cachedModel = null;
+        }
+
+        public ProductViewModel(Item? model)
+        {
+            Model = model;
+            if (model != null)
+            {
+                cachedModel = new Item(model);
+            }
+        }
+    }
+}
